401911dd276c7a493a23c6f563dbd442d7089b24
rboszko <rafa@hotmail.sk>
2019-08-17 00:47:24 +0200

    "#0006 - Change standard of submit variable (Formatting)"

--- src/gameFactory.js
+++ src/gameFactory.js
@@ -1,45 +1,45 @@
-app.factory('gameFactory', function() {
+app.factory('gameFactory', function () {
 
-  var Game = function() {
+  var Game = function () {
     this.bonuses = this.createBoard();
   };
 
-  Game.prototype.createBoard = function() {
+  Game.prototype.createBoard = function () {
     return {
-        'A1': 'tripleword', 'A4': 'doubleletter', 'A8': 'tripleword', 'A12': 'doubleletter', 'A15': 'tripleword',
-        'B2': 'doubleword', 'B6': 'tripleletter', 'B10': 'tripleletter', 'B14': 'doubleword',
-        'C3': 'doubleword', 'C7': 'doubleletter', 'C9': 'doubleletter', 'C13': 'doubleword',
-        'D1': 'doubleletter', 'D4': 'doubleword', 'D8': 'doubleletter', 'D12': 'doubleword', 'D15': 'doubleletter',
-        'E5': 'doubleword', 'E11': 'doubleword',
-        'F2': 'tripleletter', 'F6': 'tripleletter', 'F10': 'tripleletter', 'F14': 'tripleletter',
-        'G3': 'doubleletter', 'G7': 'doubleletter', 'G9': 'doubleletter', 'G13': 'doubleletter',
-        'H1': 'tripleword', 'H4': 'doubleletter', 'H8': 'star', 'H12': 'doubleletter', 'H15': 'tripleword',
-        'I3': 'doubleletter', 'I7': 'doubleletter', 'I9': 'doubleletter', 'I13': 'doubleletter',
-        'J2': 'tripleletter', 'J6': 'tripleletter', 'J10': 'tripleletter', 'J14': 'tripleletter',
-        'K5': 'doubleword', 'K11': 'doubleword',
+      'A1': 'tripleword', 'A4': 'doubleletter', 'A8': 'tripleword', 'A12': 'doubleletter', 'A15': 'tripleword',
+      'B2': 'doubleword', 'B6': 'tripleletter', 'B10': 'tripleletter', 'B14': 'doubleword',
+      'C3': 'doubleword', 'C7': 'doubleletter', 'C9': 'doubleletter', 'C13': 'doubleword',
+      'D1': 'doubleletter', 'D4': 'doubleword', 'D8': 'doubleletter', 'D12': 'doubleword', 'D15': 'doubleletter',
+      'E5': 'doubleword', 'E11': 'doubleword',
+      'F2': 'tripleletter', 'F6': 'tripleletter', 'F10': 'tripleletter', 'F14': 'tripleletter',
+      'G3': 'doubleletter', 'G7': 'doubleletter', 'G9': 'doubleletter', 'G13': 'doubleletter',
+      'H1': 'tripleword', 'H4': 'doubleletter', 'H8': 'star', 'H12': 'doubleletter', 'H15': 'tripleword',
+      'I3': 'doubleletter', 'I7': 'doubleletter', 'I9': 'doubleletter', 'I13': 'doubleletter',
+      'J2': 'tripleletter', 'J6': 'tripleletter', 'J10': 'tripleletter', 'J14': 'tripleletter',
+      'K5': 'doubleword', 'K11': 'doubleword',
 
-        'M1': 'doubleletter', 'M4': 'doubleword', 'M8': 'doubleletter', 'M12': 'doubleword', 'M15': 'doubleletter',
-        'N3': 'doubleword', 'N7': 'doubleletter', 'N9': 'doubleletter', 'N13': 'doubleword',
-        'O2': 'doubleword', 'O6': 'tripleletter', 'O10': 'tripleletter', 'O14': 'doubleword',
-        'P1': 'tripleword', 'P4': 'doubleletter', 'P8': 'tripleword', 'P12': 'doubleletter', 'P15': 'tripleword',
-        'Q1': 'tripleword', 'Q4': 'doubleletter', 'Q8': 'tripleword', 'Q12': 'doubleletter', 'Q15': 'tripleword'
-      };
+      'M1': 'doubleletter', 'M4': 'doubleword', 'M8': 'doubleletter', 'M12': 'doubleword', 'M15': 'doubleletter',
+      'N3': 'doubleword', 'N7': 'doubleletter', 'N9': 'doubleletter', 'N13': 'doubleword',
+      'O2': 'doubleword', 'O6': 'tripleletter', 'O10': 'tripleletter', 'O14': 'doubleword',
+      'P1': 'tripleword', 'P4': 'doubleletter', 'P8': 'tripleword', 'P12': 'doubleletter', 'P15': 'tripleword',
+      'Q1': 'tripleword', 'Q4': 'doubleletter', 'Q8': 'tripleword', 'Q12': 'doubleletter', 'Q15': 'tripleword'
+    };
   };
 
-  Game.prototype.createBag = function() {
+  Game.prototype.createBag = function () {
     var letters = _.keys(letterValues);
     var bag = [];
     for (var letter in letters) {
-      for (i = 0; i < letterValues[letters[letter]].tiles; i ++) {
+      for (i = 0; i < letterValues[letters[letter]].tiles; i++) {
         bag.push(letters[letter]);
       }
     }
     return _.shuffle(bag);
   };
 
-  Game.prototype.distributeLetters = function(currentLetters, bag) {
+  Game.prototype.distributeLetters = function (currentLetters, bag) {
     var number = 7 - currentLetters.length;
-    for (i = 0; i < number; i ++) {
+    for (i = 0; i < number; i++) {
       var x = Math.floor((Math.random() * bag.length));
       var letter = bag.splice(x, 1).join();
       currentLetters.push({ 'value': letter, 'status': 'ready' });
@@ -47,14 +47,14 @@ app.factory('gameFactory', function() {
     return currentLetters;
   };
 
-  Game.prototype.swapLetter = function(currentLetters, bag) {
+  Game.prototype.swapLetter = function (currentLetters, bag) {
     var index = _.indexOf(_.pluck(currentLetters, 'status'), 'selected');
     currentLetters.splice(index, 1);
     currentLetters = this.distributeLetters(currentLetters, bag);
     return currentLetters;
   };
 
-  Game.prototype.getPoints = function(input) {
+  Game.prototype.getPoints = function (input) {
     var currentBonuses = { 'word': 1 };
     var total = 0;
     for (var i in input) {
@@ -70,7 +70,7 @@ app.factory('gameFactory', function() {
     return total;
   };
 
-  Game.prototype.getBonuses = function(position, currentBonuses) {
+  Game.prototype.getBonuses = function (position, currentBonuses) {
     if (_.has(this.bonuses, position) === true) {
       position = this.bonuses[position];
       if (position === 'doubleword') { currentBonuses.word *= 2; }
@@ -81,12 +81,12 @@ app.factory('gameFactory', function() {
     return currentBonuses;
   };
 
-  Game.prototype.checkForBlankOrLetter = function(tile) {
+  Game.prototype.checkForBlankOrLetter = function (tile) {
     if (tile.blank === true) { return 'blank'; }
     return tile.letter;
   };
 
-  Game.prototype.bingoBonus = function(input, total) {
+  Game.prototype.bingoBonus = function (input, total) {
     if (input.length === 7) { total += 50; }
     return total;
   };
