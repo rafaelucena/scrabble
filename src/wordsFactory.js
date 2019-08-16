9e4e71522c945e7bbfffb9f08d3682e38b542806
rboszko <rafa@hotmail.sk>
2019-08-17 00:52:26 +0200

    "#0007 (fix) - Improve compound word roadblock (Formatting)"

--- src/wordsFactory.js
+++ src/wordsFactory.js
@@ -1,44 +1,44 @@
-app.factory('wordsFactory', ['$http', function($http) {
+app.factory('wordsFactory', ['$http', function ($http) {
 
-  var Words = function() {};
+  var Words = function () { };
 
-  Words.prototype.removePlacedLetters = function(playerLetters) {
-    return _.reject(playerLetters, function(letter) {
+  Words.prototype.removePlacedLetters = function (playerLetters) {
+    return _.reject(playerLetters, function (letter) {
       return letter.status === 'placed';
     });
   };
 
-  Words.prototype.organiseSubmission = function(letters) {
+  Words.prototype.organiseSubmission = function (letters) {
     letters = _.chain(letters)
       .sortBy('position')
-      .groupBy(function(letter) { return letter.position.length; })
+      .groupBy(function (letter) { return letter.position.length; })
       .value();
-    return _.flatten([ _.values(letters[2]), _.values(letters[3]) ]);
+    return _.flatten([_.values(letters[2]), _.values(letters[3])]);
   };
 
-  Words.prototype.addSelectedClass = function(letters, index) {
+  Words.prototype.addSelectedClass = function (letters, index) {
     letters[index].status = 'selected';
     return letters;
   };
 
-  Words.prototype.addPlacedClass = function(letters) {
-    return _.each(letters, function(letter) {
+  Words.prototype.addPlacedClass = function (letters) {
+    return _.each(letters, function (letter) {
       if (letter.status === 'selected') {
         letter.status = 'placed';
       }
     });
   };
 
-  Words.prototype.removeAllPlacedClasses = function(letters) {
-    return _.each(letters, function(letter) {
+  Words.prototype.removeAllPlacedClasses = function (letters) {
+    return _.each(letters, function (letter) {
       if (letter.status === 'placed') {
         letter.status = 'ready';
       }
     });
   };
 
-  Words.prototype.removeAllSelectedClass = function(letters) {
-    return _.each(letters, function(letter) {
+  Words.prototype.removeAllSelectedClass = function (letters) {
+    return _.each(letters, function (letter) {
       if (letter.status === 'selected') {
         letter.status = 'ready';
       }
